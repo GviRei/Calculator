930ee995194836d11d38c1def511e7feaeb103af
Gvidas Reistaitis <gvidasreistaitis4@gmail.com>
2017-02-22 11:15:10 +0200

    Added all files

--- app/src/main/java/com/example/tavomama/calculator/MainActivity.java
+++ app/src/main/java/com/example/tavomama/calculator/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.tavomama.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonOnClick();
+    }
+
+    public void buttonOnClick() {
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txt = (TextView) findViewById(R.id.textView);
+                txt.setText("1");
+            }
+        });
     }
 }
